6f1ccc160713b6369b587f0771136dc22ecaa1e3
JanosVargaBME <var.janos.hu@gmail.com>
2022-05-15 12:02:55 +0200

    RDFCollection BDDtest class added to #6 issue

---
+++ SpecFlowRDFSharp/Features/RDFCollection.feature
@@ -0,0 +1,7 @@
+﻿Feature: RDFCollection
+
+Tests the RDFCollection class and a subset of it's methods
+
+@tag1collection
+Scenario: Testing Collection class
+	Given Create Collection
---
+++ SpecFlowRDFSharp/StepDefinitions/RDFCollectionStepDefinition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RDFSharp.Model;
+
+namespace SpecFlowRDFSharp.StepDefinitions
+{
+    [Binding]
+    internal class RDFCollectionStepDefinition
+    {
+        private RDFCollection? collection;
+
+        /// <summary>
+        /// Tests the default constructor of the RDFCollection class.
+        /// </summary>
+        [Given("Create Collection")]
+        public void GivenCreateRDFCollection()
+        {
+            collection = new RDFCollection(RDFModelEnums.RDFItemTypes.Resource);
+
+            Assert.NotNull(collection);
+
+            Assert.Equal(collection.ItemType.ToString(), RDFModelEnums.RDFItemTypes.Resource.ToString());
+        }
+
+
+    }
+}
