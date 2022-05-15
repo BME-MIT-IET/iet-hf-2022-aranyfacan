2260167255aeecff47f4f756bb0b202e08516fcc
MokosZalan <zalanmokos@gmail.com>
2022-05-15 18:49:15 +0200

    Benchmark project created

---
+++ Benchmark/Benchmarks.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+using System;
+
+namespace Benchmark
+{
+    public class Benchmarks
+    {
+
+        RDFGraph graph1 = new RDFGraph();
+        RDFGraph graph2 = new RDFGraph();
+
+        [Benchmark(Baseline = true)]
+        public void Scenario1()
+        {
+            
+        }
+
+        [Benchmark]
+        public void Scenario2()
+        {
+            // Implement your benchmark here
+        }
+    }
+}
