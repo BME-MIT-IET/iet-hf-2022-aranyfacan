cfd1c0020a1765f54596475ff937aac827b515f6
MokosZalan <zalanmokos@gmail.com>
2022-05-19 10:24:55 +0200

    Small changes to benchmark and doc

--- RDFSharp.NonFunctionalTest/Benchmarks.cs
+++ RDFSharp.NonFunctionalTest/Benchmarks.cs
@@ -57,40 +57,59 @@ namespace RDFSharp.NonFunctionalTest
             var graphNTrples = RDFGraph.FromFile(ntriplesFormat, @"..\..\..\..\..\..\..\..\ntriples.nt");
         }
 
+        // Create triple with resource, vocabulary, typedliteral
         [Benchmark]
         public void Scenario5()
         {
-            RDFGraph graph1_1 = new RDFGraph();
-            RDFGraph graph1_2 = new RDFGraph();
-            RDFGraph graph2 = new RDFGraph();
-            RDFGraph graph3 = new RDFGraph();
+            RDFGraph graph = new RDFGraph();
 
             for (int i = 0; i < 100; i++)
             {
-                var resource1_1 = new RDFResource(RDFVocabulary.RDF.BASE_URI + "resource" + i);
-                var vocabulary1_1 = RDFVocabulary.RDF.TYPE;
+                var resource = new RDFResource(RDFVocabulary.RDF.BASE_URI + "resource" + i);
+                var vocabulary = RDFVocabulary.RDF.TYPE;
                 var typedLiteral = new RDFTypedLiteral("true", RDFModelEnums.RDFDatatypes.XSD_BOOLEAN);
-                graph1_1.AddTriple(new RDFTriple(resource1_1, vocabulary1_1, typedLiteral));
+                graph.AddTriple(new RDFTriple(resource, vocabulary, typedLiteral));
             }
+        }
+
+        // Create triple with resource, vocabulary, plainliteral
+        [Benchmark]
+        public void Scenario6()
+        {
+            RDFGraph graph = new RDFGraph();
 
             for (int i = 0; i < 100; i++)
             {
-                var resource1_2 = new RDFResource(RDFVocabulary.RDFSHARP.BASE_URI + "resource" + i);
-                var vocabulary1_2 = RDFVocabulary.RDF.VALUE;
+                var resource = new RDFResource(RDFVocabulary.RDFSHARP.BASE_URI + "resource" + i);
+                var vocabulary = RDFVocabulary.RDF.VALUE;
                 var plainLiteral = new RDFPlainLiteral("literal");
-                graph1_2.AddTriple(new RDFTriple(resource1_2, vocabulary1_2, plainLiteral));
+                graph.AddTriple(new RDFTriple(resource, vocabulary, plainLiteral));
             }
+        }
+
+        // Create container with containertypes, itemtypes
+        [Benchmark]
+        public void Scenario7()
+        {
+            RDFGraph graph = new RDFGraph();
 
             for (int i = 0; i < 100; i++)
             {
                 var container = new RDFContainer(RDFModelEnums.RDFContainerTypes.Seq, RDFModelEnums.RDFItemTypes.Resource);
-                graph2.AddContainer(container);
+                graph.AddContainer(container);
             }
+        }
+
+        // Create collection with itemtypes
+        [Benchmark]
+        public void Scenario8()
+        {
+            RDFGraph graph = new RDFGraph();
 
             for (int i = 0; i < 100; i++)
             {
                 var collection = new RDFCollection(RDFModelEnums.RDFItemTypes.Resource);
-                graph3.AddCollection(collection);
+                graph.AddCollection(collection);
             }
         }
     }
--- doc/technology_nonfunctional.md
+++ doc/technology_nonfunctional.md
@@ -5,7 +5,7 @@ BenchmarkDotNet-et használtuk a benchmarkok készítéséhez és futtatásához
 Készettettünk egy új projektet a teszteknek, ehhez telepítettük a megfelelő nuget csomagot.
 A teszteket visual studioban futtatuk, futás közben a konzolra írta, hogy éppen mit csinál, majd végül az eredményeket.
 
- Kétféle funciót teszteltünk: Az RDFSharp fájlból gráfot készítő funkcióját, és a scenario2-ben az RDF model triple, container és collection elemeinek létrehozását.
+ Kétféle funciót teszteltünk: Az RDFSharp fájlból gráfot készítő funkcióját, és az RDF model triple, container és collection elemeinek létrehozását.
  
  ### Fájlból beolvasás
  
@@ -15,6 +15,9 @@ A teszteket visual studioban futtatuk, futás közben a konzolra írta, hogy ép
  Minden teszt külön metódust kell létrehozni, [Benchmark] attribútummal. Az osztály elé írt [SimpleJob] attribútummal lehet az egyes metódusok futásainak számát be
  llítani, ezt meg is tettük. Készítettem egy [GlobalSetup] függvényt is, ami az egész tesztelés eklőtt fut le, itt gondoskodom róla, hogy a fájlok
  biztosan olvashatóak legyenek.
+
+### Triple, container és collection
+Az RDF model triple, container és collection elemeit teszteljük. Az elemeket egy-egy for-cikluson belül hozzuk létre, majd hozzáadjuk őket egy gráfhoz. A ciklusok 100-szor futnak le. A triple-t két hasonló módon is teszteljük: egyszer Resource, Vocabulary és TypedLiteral, egyszer pedig Resource, Vocabulary és PlainLiteral típust adunk a konstruktorának. A container konstruktorában megadjuk a ContainerTypest és az ItemTypest. Végül, a collection konstruktorában csupán egy ItemTypes-ra van szükség.
  
  ### Eredmények
  
@@ -33,9 +36,9 @@ viszont érdekes, hogy a szórása enek a legkevesebb, azaz kicsi az eltérés a
 
 Összességében az nTriples szerepelt a legrosszabbul, nem sok értelmét látom a használatának.
 Memóriában az xml a nyerő, sebességben a trix, de nem olyan nagy az eltérés.
+
+Elemek létrehozásánál az elvárásoknak megfelelően a collection szerepelt a legjobban, ez jóval kevesebb időbe telt, mint a többi. A PlainLiterallal létrehozott triple és a container szinte ugyanannyi időt vettek igénybe, míg a TypedLiterallal létrehozott triple egy kicsivel többet, ám ez az eltérés jóval kisebb, mint a collection-é.
  
  Warningok, hintek, jelmagyarázat, összfutásidő:
  ![](4.png)
  
- ### Triple, container és collection
-A scenario5-ben az RDF model triple, container és collection elemeit teszteljük. Az elemeket egy-egy for-cikluson belül hozzuk létre, majd hozzáadjuk őket egy gráfhoz. A ciklusok 100-szor futnak le. A triple-t két hasonló módon is teszteljük: egyszer Resource, Vocabulary és TypedLiteral, egyszer pedig Resource, Vocabulary és PlainLiteral típust adunk a konstruktorának. A container konstruktorában megadjuk a ContainerTypest és az ItemTypest. Végül, a collection konstruktorában csupán egy ItemTypes-ra van szükség.
